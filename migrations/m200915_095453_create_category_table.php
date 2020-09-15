0eb008caa30f0b2f2e6d06219b87c97665a86f22
Vladimir Drobyshev <dvvinfo@mail.ru>
2020-09-15 14:49:25 +0300

    Создал структуру блога

---
+++ migrations/m200915_095453_create_category_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m200915_095453_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%category}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%category}}');
+    }
+}
